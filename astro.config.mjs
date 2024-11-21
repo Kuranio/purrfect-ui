16d2f0214416efff61a95ba20cc655b3489029bd
kuranio <garciadiazraul2411@gmail.com>
2024-11-21 14:31:37 +0000

    removed lerna

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,14 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false,
-  })]
+  })],
+
+  output: 'server',
+  adapter: netlify()
 });
